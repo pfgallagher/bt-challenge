4f2c10e55c2ec44c4e708112acd6a38e941f7a94
pfgallagher <pfgallagher@gmail.com>
2019-05-05 15:17:43 -0500

    Create proxy endpoint for secure API calls using Express. Closes #3

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
-const giphyAPIKey = process.env.GIPHY_API_KEY;
-
 const router = require("express").Router();
 module.exports = router;
+
+router.use("/search", require("./search"));
 
 router.use((req, res, next) => {
 	const error = new Error("Not Found");
