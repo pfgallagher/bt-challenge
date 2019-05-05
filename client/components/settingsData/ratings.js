f3d4376e338cab0581d9ed57b562128cf1e1b528
pfgallagher <pfgallagher@gmail.com>
2019-05-05 17:19:08 -0500

    Create settings reducer to track language and rating setting. Connect reducer to selector on main page. Closes #10, closes #13, closes #15, closes #16, and closes #24

---
+++ client/components/settingsData/ratings.js
@@ -0,0 +1,9 @@
+const ratings = [
+	{ key: "Y", text: "Y", value: "y" },
+	{ key: "G", text: "G", value: "g" },
+	{ key: "PG", text: "PG", value: "pg" },
+	{ key: "PG-13", text: "PG-13", value: "pg-13" },
+	{ key: "R", text: "R", value: "r" },
+];
+
+export default ratings;
